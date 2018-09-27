6573d2b2ba11ac1856bf09354a878ee8f4005d7e
ho2921ho <ho2921ho@naver.com>
2018-09-27 09:17:51 +0900

    9.27

---
+++ 4MAT 클러스터링/4mat.r
@@ -0,0 +1,85 @@
+rm(list=ls());gc()
+
+## data
+
+setwd("C:/Users/renz/Desktop/4MAT 클러스터링")
+test = read.csv("test.csv", header = T)
+test = na.omit(test)
+
+names(test)
+
+
+rownames(test) = test[,"이름"]
+test = test[,-1]
+
+scale.test = scale(test[,-c(6,7)])
+team = test$팀
+sex = test$X
+#---- 계층적 군집분석
+
+# (1) Euclidean dist., complete <- default option
+
+dist.E = dist(scale.test)
+cluster.EC = hclust(dist.E) 
+plot(cluster.EC, main="Euclidean, complete", cex=1)
+rect.hclust(cluster.EC, k=4, border="red") 
+
+# (2) Euclidean dist., average linkage
+
+cluster.EA = hclust(dist.E, method="average")
+plot(cluster.EA, main="Euclidean, average")
+rect.hclust(cluster.EA, k=4, border="red") 
+
+#--- 2-3. K-means 군집분석(p.73)
+
+
+# K-means
+library(cluster)
+set.seed(1)
+cluster.K4 = kmeans(scale.test, centers= 4, nstart = 50)
+cluster.K4$cluster
+table(cluster.K4$cluster)
+
+#--- 다차원 척도법을 이용한 군집분석 시각화(p.81)
+k1 = 4
+cmds = cmdscale(dist.E, k = 2)
+
+groups = cutree(cluster.EC, k=k1)
+plot(cmds, xlab = "coord 1", ylab = "coord 2", main = "MDS-hclust", type = "n")
+text(cmds, labels = rownames(cmds), cex=.8, col=groups)
+
+plot(cmds, xlab = "coord 1", ylab = "coord 2", main = "MDS-kmeans", type = "n")
+text(cmds, labels = rownames(cmds), cex=.8, col=cluster.K4$cluster)
+
+plot(cmds, xlab = "coord 1", ylab = "coord 2", main = "MDS-team", type = "n")
+text(cmds, labels = rownames(cmds), cex=.8, col=team)
+
+plot(cmds, xlab = "coord 1", ylab = "coord 2", main = "MDS-team", type = "n")
+text(cmds, labels = rownames(cmds), cex=.8, col=sex)
+## k-means 
+
+#--- PCA 를 이용한 군집분석 시각화(p.78)
+
+pca <- princomp(scale.test, cor=T)
+
+groups = cutree(cluster.EC, k=k1)
+plot(pca$scores[,1:2], xlab = "coord 1", ylab = "coord 2", main = "PCA-hclust", type = "n")
+text(pca$scores[,1:2], labels = rownames(pca$scores), cex=.8, col=groups)
+
+plot(pca$scores[,1:2], xlab = "coord 1", ylab = "coord 2", main = "PCA-kmeans", type = "n")
+text(pca$scores[,1:2], labels = rownames(pca$scores), cex=.8, col=cluster.K4$cluster)
+
+plot(pca$scores[,1:2], xlab = "coord 1", ylab = "coord 2", main = "PCA-team", type = "n")
+text(pca$scores[,1:2], labels = rownames(cmds), cex=.8, col=team)
+
+## 군집 내 비교 (k-means)
+
+groups = cluster.K4$cluster
+for (i in 1:4){
+  cat(i,'- th cluster \n')
+  tmp = rbind(round(colMeans(test[,-6][which(groups==i),]), 3),
+              round(diag(cov(test[,-6][which(groups==i),])), 3)); rownames(tmp) = c("Mean","Var")
+  print(tmp)
+}
+table(groups)
+
---
+++ 동아리_작업일정표_만들기/min_var.py
@@ -0,0 +1,135 @@
+import string
+import pandas as pd
+import numpy
+
+min_var_old_cand = []
+min_var_new_cand = []
+temp = 0
+
+for i in range(1,101):
+
+        
+        gardening_data = open('gardening_data.txt', mode= 'r', encoding = 'ANSI').readlines()
+
+        name = []
+
+        isold = []
+
+        date = []
+        # 회원별 급수가능한 날짜를 담기 위해 빈리스트를 생성한다.
+
+
+        ## 2단계 for문을 통해 리스트 채우기.
+
+        import random
+
+        pre_row_list = []
+        for row in gardening_data:
+            row = row.rstrip().split(',')
+            pre_row_list.append((str(row)))
+            random.shuffle(pre_row_list)
+            
+        row_list = []
+        for i in pre_row_list:
+            for t in string.punctuation:
+                i = i.replace(t,'')
+            i = i.split(' ')
+            row_list.append(i)
+
+            
+        # 변환된 list를 활용해서 앞서 만든 빈 리스트들을 채운다.
+        for line in row_list:
+            name.append(line[0])
+            # 이름 리스트에는 line의 인덱스가 0인 값만을 담아서 이름 명단이 채워지게 한다.
+            isold.append(line[1])
+            # 기존회원여부 리스트에는 line의 인덱스 1인 값만을 담아서 기존회원여부 리스트를 만든다.
+            date.append(line[2:])
+            # date에는 나머지 항목을 넣는다.
+
+        ## 3단계 급수일자를 중복없이 빼오기
+        unique_date = set()
+        # 급수일자를 '중복없이' 빼오기 위해 빈 셋을 생성한다.
+        for i in date:
+            for j in i:
+                unique_date.add(int(j))
+
+        unique_date = list(unique_date)
+        unique_date.sort()
+        # 이중 for문을 통해 date에서 모든 항목을 뽑아놓고 set() 넣었다.
+        # 정렬을 위해 list로 형을 변환했다. 
+
+
+        ## 4단계 디폴트 딕셔너리에 값을 채워 넣기
+
+        from collections import defaultdict
+        # defaultdict 자료형을 이용해서 참조하려는 키가 없을 경우. 키를 생성하고
+        # 값을 리스트 자료형으로 가질 수 있게 했다. 
+
+        gardening_dict = defaultdict(list)
+        # 디폴트 딕셔너리 만듬.
+        # 최소분산을 구하자!!
+        ## 4-1단계  기존회원과 하루만 가능한 사람들 처리
+        for i, idi in enumerate(name):
+            if isold[i] == '1' and len(date[i]) == 1:
+            # 회원구분이 기존이고 하루밖에 가능하지 않을 때.
+                gardening_dict[str(date[i]).strip('[]\'')].append(idi)
+                # 디폴트 딕셔너리에 날짜로 키를 만들고 이름을 문자열 항목으로 담은 리스트를 값으로 대응시켰다.
+            if isold[i] == '1' and len(date[i]) != 1:
+            # 회원구분이 기존이고 여러날짜가 가능할 때.
+                    for j in date[i]:
+                        find = 0
+                        if len(gardening_dict[j]) == 0:
+                            gardening_dict[j] = [idi]
+                            find += 1
+                        if find == 1:
+                            break
+                    else:
+                        min_candidate = []
+                        for j in date[i]:
+                            min_candidate.append(len(gardening_dict[j]))
+                        min_date = min(min_candidate)
+                        for k in date[i]:
+                            if len(gardening_dict[k]) == min_date:
+                                gardening_dict[k].append(idi)
+                                break
+
+        o_values = []
+        for i in gardening_dict.values(): 
+                o_values.append(len(i))
+        
+        o_var_values = round(numpy.var(o_values),6)
+        min_var_old_cand.append(o_var_values)
+
+        # 4-2 하루만 가능한 신입 회원의 처리
+        for i, idj in enumerate(name):
+         if isold[i] == '0' and len(date[i]) == 1:
+
+                gardening_dict[str(date[i]).strip('[]\'')].append(idj)
+                # 기존과 동일
+
+        ## 4-3 단계 여러 날짜가 가능한 신입회원의 처리.
+
+
+        for i, idx in enumerate(name):
+
+            if isold[i] == '0' and len(date[i]) != 1:
+            # 회원구분이 신입이이고 여러날짜가 가능할 때
+                min_candidate = []
+                for j in date[i]:
+                    min_candidate.append(len(gardening_dict[j]))
+                min_date = min(min_candidate)
+                for k in date[i]:
+                    if len(gardening_dict[k]) == min_date:
+                        gardening_dict[k].append(idx)
+                        break
+
+        # 5단계 급수표를 수치화해서 최소분산 구하기.
+        n_values = []
+        for i in gardening_dict.values():
+                n_values.append(len(i))
+        
+        n_var_values = numpy.var(n_values)
+        min_var_new_cand.append(n_var_values)
+
+min_var_old = min(min_var_old_cand)
+min_var_new = min(min_var_new_cand)
